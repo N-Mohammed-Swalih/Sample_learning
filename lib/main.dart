08d5ff3fec356813a8c66dc4c6ca8c6a4d65acfc
N Mohammed Swalih <137269292+N-Mohammed-Swalih@users.noreply.github.com>
2023-07-11 13:31:17 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import './widgets/listview.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Sample_Project",
+      home: Scaffold(
+        body: listView(),
+          ),
+        ),
+  );
+}
